54fd0d62de082e6cb174d7fe6bf07ee81e1a0409
Yashwanth-dr <yashwanthdr5@gmail.com>
2026-04-27 13:19:33 +0530

    Refactor root rendering in index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// We only need to define 'root' once
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// We only call render once
 root.render(
   <React.StrictMode>
     <App />
